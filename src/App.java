eeb6da142e166d3bd4c0c7018a76eb062d5c781a
Yury Korsun <Zmey_85@bk.ru>
2017-11-29 10:44:38 +0300

    Ура! Наконец-то доделал!

--- src/App.java
+++ src/App.java
@@ -1,67 +1,116 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class App {
+public class App extends JFrame{
+
+    private JTextArea inOut;
+    private JButton encoder, decoder;
+    private String inString, outString;
+
+
+    public App() {
+        setTitle("Шифровщик сообщений");
+        setSize(620,200);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        inOut = new JTextArea(10,30);
+        inOut.setLineWrap(true);
+        JScrollPane scrollPane = new JScrollPane(inOut);
+        encoder = new JButton("Зашифровать");
+        encoder.addActionListener(new EncoderActionListener());
+
+        decoder = new JButton("Расшифровать");
+        decoder.addActionListener(new DecoderActionListener());
+
+        //add(inOut);
+        add(scrollPane);
+        add(encoder);
+        add(decoder);
+
+        setVisible(true);
+    }
+
     public static void main(String[] args) {
 
-        String in = "Привет ЮРИК и тебе";
-        System.out.println(in);
-        String buf;
-        buf = in.toLowerCase();
+        new App();
+    }
 
-        char[] letter;
-        letter = buf.toCharArray();
+    private class EncoderActionListener implements ActionListener {
 
-        String out = "";
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            inString = inOut.getText();
 
-        for (int i = 0; i < letter.length; i++) {
-            if ((int)letter[i] == 32) {
-                out += " || ";
-            } else {
-                out += ((int)letter[i] - 1071) + " ";
+            String buf;
+            buf = inString.toLowerCase();
+
+            char[] letter;
+            letter = buf.toCharArray();
+
+            String outString = "";
+
+            for (int i = 0; i < letter.length; i++) {
+                outString += ((int)letter[i]) + " ";
             }
 
+            inOut.setText(outString);
         }
+    }
 
-        System.out.println(out);
+    private class DecoderActionListener implements ActionListener {
 
-        Pattern pattern = Pattern.compile("([\\d\\s]+)|([^\\s||\\s])");
+        @Override
+        public void actionPerformed(ActionEvent e) {
 
-        Matcher matcher = pattern.matcher(out);
+            outString = inOut.getText();
 
-        List<String> convertWords = new ArrayList<>();
+            Pattern pattern = Pattern.compile("([\\d\\s]+)|([^\\n])");
 
-        while (matcher.find()) {
-            convertWords.add(matcher.group());
-        }
+            Matcher matcher = pattern.matcher(outString);
 
-        for (int i = 0; i < convertWords.size(); i++) {
-            System.out.println(convertWords.get(i));
-        }
+            List<String> convertWords = new ArrayList<>();
 
-        List<String> nums = new ArrayList<>();
-
-        Pattern pattern1 = Pattern.compile("[\\d]+[\\u2000]");
-
-        for (int i = 0; i < convertWords.size(); i++) {
-
-            Matcher matcher1 = pattern1.matcher(convertWords.get(i));
-
-            while (matcher1.find()) {
-                nums.add(matcher1.group());
+            while (matcher.find()) {
+                convertWords.add(matcher.group());
             }
-        }
 
-        List<Integer> numLetter = new ArrayList<>();
-        for (int i = 0; i < nums.size(); i++) {
-            numLetter.add(Integer.parseInt(nums.get(i)));
-        }
+            List<String> nums = new ArrayList<>();
 
+            Pattern pattern1 = Pattern.compile("[\\d]+");
 
-        for (int i = 0; i < numLetter.size(); i++) {
-            System.out.println((char)(numLetter.get(i)+1071));
+            for (int i = 0; i < convertWords.size(); i++) {
+
+                Matcher matcher1 = pattern1.matcher(convertWords.get(i));
+
+                while (matcher1.find()) {
+                    nums.add(matcher1.group());
+                }
+            }
+
+            List<Integer> numLetter = new ArrayList<>();
+
+            for (int i = 0; i < nums.size(); i++) {
+                numLetter.add(Integer.parseInt(nums.get(i)));
+            }
+
+            String temp = "";
+
+            for (int i = 0; i < convertWords.size(); i++) {
+                Matcher matcher1 = pattern1.matcher(convertWords.get(i));
+                while (matcher1.find()) {
+                    temp += ( String.valueOf((char) (Integer.parseInt(matcher1.group()))));
+                }
+            }
+
+            inOut.setText(temp);
         }
     }
 }
